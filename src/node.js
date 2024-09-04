3be9d728bcc483f85fa79c680d63d30146dd2932
slpkid-laptop <153894144+slpkid@users.noreply.github.com>
2024-09-04 15:48:45 -0700

    Add escape case for buildTree if function is accessing an out of range value

--- src/node.js
+++ src/node.js
@@ -5,42 +5,127 @@ function Node(data) {
 }
 
 function Tree(array) {
+  // sort the given array
+  const compareNumbers = (a, b) => {
+    return a - b;
+  };
 
-    const compareNumbers = (a,b) => {return a - b}
+  // sort the provided array
+  array = array.sort(compareNumbers);
 
-    array = array.sort(compareNumbers)
+  console.log(array)
+  
+  // use recursion to build the balance binary search tree
+  // requires a sorted array
+  const buildTree = (array, start = 0, end = array.length) => {
+    if (start > end) return null;
+    const mid = (start + end) / 2;
 
-    // console.log(array)
+    //escape if it's trying to access an out of range value.
+    if (mid === array.length) return null
+    let root = new Node(array[mid]);
     
-    const buildTree = (array, start = 0, end = array.length) => {
-        if (start > end) return null
-        const mid = (start + end) / 2
-        let root = new Node(array[mid])
+    root.left = buildTree(array, start, mid - 1);
+    root.right = buildTree(array, mid + 1, end);
 
-        root.left = buildTree(array, start, mid-1)
-        root.right = buildTree(array, mid+1, end)
-        return root
-        
-    }     
+    console.log(root)
+    console.log(start)
+    console.log(end)
+    console.log(mid)
 
-    let root = buildTree(array);
+    return root;
+  };
+  
+  const insert = (root, value) => {
+    // if insert is run on a null node
+    // return a new node using the value
+    if (root === null) return new Node(value);
+    
+    // if the node matches the value, end the insertion
+    if (root.data === value) return root;
+    
+    if (value < root.data) {
+      root.left = insert(root.left, value);
+    } else if (value > root.data) {
+      root.right = insert(root.right, value);
+    }
+    
+    return root;
+  };
+  
+  const getSuccessor = (current) => {
+    current = current.right;
+    while ((current !== null) & (current.left !== null)) {
+      current = current.left;
+    }
+    return current;
+  };
+  
+  const deleteItem = (root, value) => {
+    //escape case::
+    if (root === null) return root;
 
-    return { root };
+    if (value < root.data) {
+      root.left = deleteItem(root.left, value);
+    } else if (value > root.data) {
+      root.right = deleteItem(root.right, value);
+    } else {
+      if (root.left === null) {
+        return root.right
+      }
+      
+      if (root.right === null) {
+        return root.left
+      }
+
+      //when both children are present
+      const successor = getSuccessor(root);
+      root.data = successor.data;
+      root.right = deleteItem(root.right, successor.data);
+    }
+    return root;
+  };
+
+  // use return value of the BBST function for root
+  let root = buildTree(array);
+
+  return { root, insert, deleteItem, insert, getSuccessor };
 }
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
 
-const myTree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-prettyPrint(myTree.root)
+
+const myTree = Tree([1,2])
+
+// const myTree = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+console.log('Created Tree. Printing tree:')
+prettyPrint(myTree.root);
+
+// console.log("Inserting 40")
+// myTree.insert(myTree.root, 40)
+// prettyPrint(myTree.root);
+
+// console.log("Inserting 30")
+// myTree.insert(myTree.root, 30)
+// prettyPrint(myTree.root);
+
+// console.log("Inserting 6344")
+// myTree.insert(myTree.root, 6344)
+// prettyPrint(myTree.root);
+
+// console.log("Deleting undefined:")
+// myTree.deleteItem(myTree.root,undefined)
+// prettyPrint(myTree.root)
+
 //console.log('bahinga')
